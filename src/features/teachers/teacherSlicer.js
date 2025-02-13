d0f95e76a6d782da553cd7e952ab165d87a76666
Karan-Bharti1 <karan.codes1@gmail.com>
2025-02-13 20:31:08 +0530

    feat: added Delete button functionality to delete teachers data

--- src/features/teachers/teacherSlicer.js
+++ src/features/teachers/teacherSlicer.js
@@ -5,6 +5,11 @@ export const fetchTeachers=createAsyncThunk("teachers/fetchTeachers",async()=>{
 const response=await axios.get(teachersGetUrl)
 return response.data
 })
+export const   deleteTeacher=createAsyncThunk("teachers/deleteTeacher",async(id)=>{
+    const response=await axios.delete(`${teachersGetUrl}/${id}`)
+    console.log(response.data)
+    return response.data
+})
  export const teacherSlice=createSlice({
     name:"Teachers",
     initialState:{
@@ -12,7 +17,7 @@ return response.data
         status:'idle',
         error:null,
     },
-    
+ 
     reducers:{},
     extraReducers:(builder)=>{
         builder.addCase(fetchTeachers.pending,(state,action)=>{
@@ -23,6 +28,17 @@ return response.data
             state.teachers=action.payload
         })
         builder.addCase(fetchTeachers.rejected,(state,action)=>{
+            state.status="error"
+            state.error=action.payload.message
+        })
+        builder.addCase(deleteTeacher.pending,(state,action)=>{
+            state.status="loading"
+        })
+        builder.addCase(deleteTeacher.fulfilled,(state,action)=>{
+            state.status="succeeded"
+            state.teachers=state.teachers.filter(teacher=>teacher._id===action.payload.id)
+        })
+        builder.addCase(deleteTeacher.rejected,(state,action)=>{
             state.status="error"
             state.error=action.payload.message
         })
--- src/pages/ViewTeacher.jsx
+++ src/pages/ViewTeacher.jsx
@@ -1,38 +1,48 @@
 import { Link, useParams } from "react-router-dom"
 import Header from "../components/Header"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
-import { fetchTeachers } from "../features/teachers/teacherSlicer"
+import { deleteTeacher, fetchTeachers } from "../features/teachers/teacherSlicer"
 
 const ViewTeacher=()=>{
     const dispatch=useDispatch()
     const {teacherId}=useParams()
-   
+   const [deleteTeacherData,setDeleteTeacher]=useState(false)
     useEffect(()=>{
         dispatch(fetchTeachers())
     },[dispatch])
     const teachers=useSelector(state=>state.teacherSlice)
     const requiredTeacher=teachers.status==="succeeded" && teachers.teachers.find(teacher=>teacher._id===teacherId)
-    console.log(requiredTeacher)
+
+    const handleDelete=id=>{
+        dispatch(deleteTeacher(id))
+        teachers.status==="succeeded" && setDeleteTeacher(true)
+    }
     return(<>
     <Header/>
     <main className="container">
-        <h1 className="my-3">Teacher's Details</h1>
+       
+{teachers.status!=="loading" && !deleteTeacherData && (<>
+    <h1 className="my-3">Teacher's Details</h1>
+<h2>{requiredTeacher?.teacherName}</h2>
+<hr/>
+<p className="fs-4"><strong>Contact: </strong>{requiredTeacher?.phoneNumber}</p>
+<hr/>
+<p className="fs-4"><strong>Email Id: </strong>{requiredTeacher?.emailId}</p>
+<hr/>
 
-<h2>{requiredTeacher.teacherName}</h2>
+<p className="fs-4"><strong>Gender: </strong>{requiredTeacher?.gender}</p>
 <hr/>
-<p className="fs-4"><strong>Contact: </strong>{requiredTeacher.phoneNumber}</p>
+<p className="fs-4"><strong>Qualification: </strong>{requiredTeacher?.qualification}</p>
 <hr/>
-<p className="fs-4"><strong>Email Id: </strong>{requiredTeacher.emailId}</p>
+<p className="fs-4"><strong>Result Percentage: </strong>{requiredTeacher?.resultPercentage}</p>
 <hr/>
+<Link className="btn btn-warning">Edit Details</Link> <button onClick={()=>handleDelete(requiredTeacher?._id)} className="btn btn-danger mx-3">Delete</button></>)}
+{deleteTeacherData && (<div>
+<h1>Data Deleted Successfully.</h1>
+<Link to="/teachers" className="btn btn-warning">Teachers</Link>
 
-<p className="fs-4"><strong>Gender: </strong>{requiredTeacher.gender}</p>
-<hr/>
-<p className="fs-4"><strong>Qualification: </strong>{requiredTeacher.qualification}</p>
-<hr/>
-<p className="fs-4"><strong>Result Percentage: </strong>{requiredTeacher.resultPercentage}</p>
-<hr/>
-<Link className="btn btn-warning">Edit Details</Link> <button className="btn btn-danger mx-3">Delete</button>
+</div>)}
     </main>
     </>)}
     export default ViewTeacher;
